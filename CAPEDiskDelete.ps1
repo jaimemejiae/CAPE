638a56a23797e28d0fbe54e020debf8f40456353
jaimemejiae <jaimemejiae@gmail.com>
2023-03-01 16:53:52 -0500

    Agrego Funcion Remove-Disk

--- CAPEDiskDelete.ps1
+++ CAPEDiskDelete.ps1
@@ -15,11 +15,29 @@
  
 }
 
+function Remove-Disk ($disksdelete, $VM) {
+
+$disksdelete| ForEach-Object{
+
+$ID = $_.Id.Substring($_.Id.Length-4)
+$spec = New-Object VMware.Vim.VirtualMachineConfigSpec
+$spec.DeviceChange = New-Object VMware.Vim.VirtualDeviceConfigSpec[] (1)
+$spec.DeviceChange[0] = New-Object VMware.Vim.VirtualDeviceConfigSpec
+$spec.DeviceChange[0].Device = New-Object VMware.Vim.VirtualDisk
+$spec.DeviceChange[0].Device.Key = $ID
+$spec.DeviceChange[0].Operation = 'remove'
+$spec.CpuFeatureMask = New-Object VMware.Vim.VirtualMachineCpuIdInfoSpec[] (0)
+$_this = Get-View -ViewType VirtualMachine -Filter @{'Name'= $VM}
+$_this.ReconfigVM_Task($spec)
+}
+}
+
 $credentials = Get-Credential
 Connect-VIServer 172.18.40.30 -Credential $credentials
 $NodosPrimarios = "BOGPVBDWP009", "BOGPVBDWP011", "BOGPVPPWP014"
 $NodosSecundarios = "BOGPVBDWP010", "BOGPVBDWP012", "BOGPVPPWP015"
-$PowerCliContext = Get-PowerCLIContext
+#$NodosSecundarios = "BOGPVPPWP015"
+#$PowerCliContext = Get-PowerCLIContext
 $nodos = $NodosPrimarios + $NodosSecundarios
 $BusNumbers = @()
 for ($i=0; $i -lt $nodos.Length; $i++) {
@@ -27,17 +45,21 @@ for ($i=0; $i -lt $nodos.Length; $i++) {
   $BusNumbers += (Get-ScsiController -VM $nodos[$i]).Count - 1
 }
 
-  $NodosSecundarios | ForEach-Object -Parallel  {
-    #Use-PowerCLIContext -PowerCLIContext $using:PowerCliContext -SkipImportModuleChecks 
+  $NodosSecundarios | ForEach-Object  {
     Get-HardDisk -VM $_ -DiskType RawPhysical  | Remove-HardDisk -Confirm:$false
-   Get-hardDisk -VM $_ | Where-Object {($_.CapacityGB -eq '0')} | Remove-HardDisk -Confirm:$false
+    $disks =Get-hardDisk -VM $_ | Where-Object {($_.CapacityGB -eq '0')} | Select-Object Id
+    if($disks -ne $null){
+      Remove-Disk $disks $_
+    }
+
   }
+$NodosPrimarios | ForEach-Object {
 
-
-$NodosPrimarios | ForEach-Object -parallel {
-  Use-PowerCLIContext -PowerCLIContext $using:PowerCliContext -SkipImportModuleChecks
   Get-HardDisk -VM $_ -DiskType RawPhysical   | Remove-HardDisk -DeletePermanently:$true -Confirm:$false
-  Get-hardDisk -VM $_ | Where-Object {($_.CapacityGB -eq '0')}  | Remove-HardDisk -Confirm:$false  
+  $disks =Get-hardDisk -VM $_ | Where-Object {($_.CapacityGB -eq '0')} | Select-Object Id
+  if($disks -ne $null){
+    Remove-Disk $disks $_
+  }
 }
 
 
